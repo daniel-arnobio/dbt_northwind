b05bc575ba8b3401cfebee84e357f5109179b83c
Daniel Arnóbio <daniel.arnobio@indicium.tech>
2023-05-24 00:00:42 +0000

    creating stg_order_details

--- models/staging/stg_customers.sql
+++ models/staging/stg_customers.sql
@@ -1,7 +1,14 @@
 with
     customers as (
-        select *
-        from {{ source ('northwind', 'customers')}}
+        select 
+            customer_id
+            , company_name
+            , contact_name
+            , address
+            , city
+            , country
+        from {{source('northwind', 'customers')}}
     )
+
 select *
 from customers
