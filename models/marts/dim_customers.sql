94f5f5f16e20f49c6d93b3d876b131274478ae98
Daniel Arnóbio <daniel.arnobio@indicium.tech>
2023-05-24 00:22:14 +0000

    modifed dbt_projects to materialzed marts as table

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -1,7 +1,10 @@
-with dim_customers as (
-        select *
-        from {{ ref("stg_customers") }}
-)
+with
+    dim_customers as (
+        select
+            {{ dbt_utils.generate_surrogate_key(['customer_id', 'company_name']) }} as sk_customer
+            , *
+        from {{ref("stg_customers")}}
+    )
 
 select *
 from dim_customers
